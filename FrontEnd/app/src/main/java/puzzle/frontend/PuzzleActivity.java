0a02edad638cc6e889a38da94fe3cf195ba56605
Patricia Reinoso <patricia.reinoso12@gmail.com>
2018-01-04 22:49:58 +0100

    Tiles move when clicked

--- FrontEnd/app/src/main/java/puzzle/frontend/PuzzleActivity.java
+++ FrontEnd/app/src/main/java/puzzle/frontend/PuzzleActivity.java
@@ -3,11 +3,17 @@ package puzzle.frontend;
 import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
 
+import static java.sql.DriverManager.println;
+
 public class PuzzleActivity extends AppCompatActivity {
+
+    GridView board;
+    TileAdapter tiles = new TileAdapter(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +22,24 @@ public class PuzzleActivity extends AppCompatActivity {
         Resources res = getResources();
         setContentView(R.layout.activity_puzzle);
 
-        GridView board = (GridView) findViewById(R.id.Board);
-        board.setAdapter(new TileAdapter(this));
+        board = (GridView) findViewById(R.id.Board);
+        board.setAdapter(tiles);
 
         board.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                //something heere
+
+                if (tiles.canMove(position)){
+                    Log.i("MOVING " , Integer.toString((Integer) tiles.getItem(position)));
+                    tiles.moveTile(position);
+                    board.setAdapter(tiles);
+                }
+                else {
+                    Log.i("NOT MOVING ", Integer.toString((Integer) tiles.getItem(position)));
+                }
             }
         });
     }
+
+
 }
--- FrontEnd/app/src/main/java/puzzle/frontend/TileAdapter.java
+++ FrontEnd/app/src/main/java/puzzle/frontend/TileAdapter.java
@@ -1,9 +1,13 @@
 package puzzle.frontend;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+
+import java.util.ArrayList;
 
 /**
  * Created by patty on 1/4/18.
@@ -12,10 +16,29 @@ import android.widget.BaseAdapter;
 public class TileAdapter extends BaseAdapter {
     private Context mContext;
 
-    private Integer[] tiles = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0};
+    private Integer[] tiles = new Integer[SIZE];
+
+    /**
+     * Positions that can be moved.
+     */
+    private ArrayList<Integer> canMove = new ArrayList<Integer>();
+
+    private int spacePosition;
+
+
+    public static final int SIZE = 16;
+    public static final int COLUMNS = 4;
+
 
     public TileAdapter(Context c) {
         mContext = c;
+        for (int i = 0; i < SIZE - 1; i++){
+            tiles[i] = i + 1;
+        }
+        tiles[SIZE-1] = 0;
+        spacePosition = SIZE -1;
+        canMove.add(11);
+        canMove.add(14);
     }
 
     public int getCount() {
@@ -27,22 +50,64 @@ public class TileAdapter extends BaseAdapter {
     }
 
     public long getItemId(int position) {
-        return position%4;
+        return position % COLUMNS;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TileButton tile;
+        Button tile;
         if (convertView == null){
             // if it is not recycled, initialized some attributes
-            tile = new TileButton(mContext, position);
+            tile = new Button(mContext);
         }
         else {
-            tile = (TileButton) convertView;
+            tile = (Button) convertView;
         }
-        tile.setValue(tiles[position]);
+        if (tiles[position] != 0){
+            tile.setText(Integer.toString(tiles[position]));
+        }
+        else {
+            tile.setText("");
+            tile.setBackgroundColor(Color.TRANSPARENT);
+        }
+        tile.setClickable(false);
+        tile.setFocusableInTouchMode(false);
+        tile.setFocusable(false);
         return tile;
     }
 
+    public int getSpacePosition(){
+        return spacePosition;
+    }
+
+    public boolean canMove(int position){
+        return canMove.contains(position);
+    }
+
+    public void moveTile (int position){
+        if (canMove(position)){
+            // Switch tile values
+            tiles[spacePosition] = tiles[position];
+            tiles[position] = 0;
+
+            // Update the space position
+            spacePosition = position;
+
+            // Update canMove list.
+            canMove.clear();
+            if (spacePosition > 3){
+                canMove.add(spacePosition-4);
+            }
+            if ((spacePosition % 4) != 0){
+                canMove.add(spacePosition-1);
+            }
+            if (spacePosition < 12){
+                canMove.add(spacePosition+4);
+            }
+            if (((spacePosition +1) % 4) != 0){
+                canMove.add(spacePosition+1);
+            }
+        }
+    }
 
 }
